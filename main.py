ea655e293510d56467124a8089f7b93784605bbb
mame0112@gmail.com <mame0112@gmail.com>
2020-04-16 21:04:24 +0900

    Change code based on lint

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-from flask import Flask, jsonify, request
-from flask_restful import reqparse, abort, Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource
 
-from server.const.const import User, Conversation, Host, Visit, HttpResponseCode
+from server.const.const import User, Conversation, Host, Visit
 
 from server.util.logger import Logger
 
@@ -16,7 +15,7 @@ app = Flask(__name__, static_url_path='', static_folder='./app/dist/app')
 app.config['JSON_AS_ASCII'] = False
 api = Api(app)
 
-log = Logger("DatastoreManager")
+log = Logger("Main")
 
 
 class User(Resource):
@@ -28,23 +27,23 @@ class User(Resource):
         # json_data = request.get_json(force=True)
         # username = json_data[User.KEY_USER_NAME]
         # thumb_url = json_data[User.KEY_THUMB_URL]
-        dataManager = DatastoreManager()
-        return dataManager.get_user(user_id).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.get_user(user_id).get_result_json()
 
     def post(self):
         log.debug('post')
         user = request.json
 
-        dataManager = DatastoreManager()
-        result = dataManager.create_user(user)
+        data_manager = DatastoreManager()
+        result = data_manager.create_user(user)
         return result.get_result_json()
 
     def put(self):
         log.debug('put')
         user = request.json
 
-        dataManager = DatastoreManager()
-        return dataManager.update_user(user).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.update_user(user).get_result_json()
 
 
 class Visit(Resource):
@@ -53,31 +52,31 @@ class Visit(Resource):
         log.debug('get')
         visit_id = request.args['visit_id']
         log.debug(visit_id)
-        dataManager = DatastoreManager()
-        result = dataManager.get_visit(visit_id)
+        data_manager = DatastoreManager()
+        result = data_manager.get_visit(visit_id)
         return result.get_result_json()
 
     def post(self):
         log.debug('post')
         visit_data = request.json
 
-        dataManager = DatastoreManager()
-        return dataManager.create_visit(visit_data).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.create_visit(visit_data).get_result_json()
 
 
 class Host(Resource):
 
     def get(self):
         log.debug('get')
-        host_data = request.json
-        dataManager = DatastoreManager()
-        return dataManager.get_host().get_result_json()
+        # host_data = request.json
+        data_manager = DatastoreManager()
+        return data_manager.get_host().get_result_json()
 
     def post(self):
         log.debug('post')
         host_data = request.json
-        dataManager = DatastoreManager()
-        return dataManager.create_host(host_data).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.create_host(host_data).get_result_json()
 
 
 class Conversation(Resource):
@@ -86,20 +85,20 @@ class Conversation(Resource):
         log.debug('Conversation get')
         conversation_id = request.args['conversation_id']
         log.debug(conversation_id)
-        dataManager = DatastoreManager()
-        return dataManager.get_conversation(conversation_id).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.get_conversation(conversation_id).get_result_json()
 
     def post(self):
         log.debug('Conversation post')
         conversation_data = request.json
-        dataManager = DatastoreManager()
-        return dataManager.create_conversation(conversation_data).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.create_conversation(conversation_data).get_result_json()
 
     def put(self):
         log.debug('Conversation put')
         conversation_data = request.json
-        dataManager = DatastoreManager()
-        return dataManager.update_conversation(conversation_data).get_result_json()
+        data_manager = DatastoreManager()
+        return data_manager.update_conversation(conversation_data).get_result_json()
 
 
 class Comment(Resource):
@@ -115,17 +114,8 @@ class Comment(Resource):
     def put(self):
         log.debug('Comment put')
         comment_data = request.json
-        dataManager = DatastoreManager()
-        return dataManager.update_comment(comment_data).get_result_json()
-
-
-#     def post(self):
-#         log.debug('Webhook post')
-#         log.debug(request.get_data())
-#         log.debug(request.args.get("hub.challenge"))
-#         log.debug(request.args.get("hub.mode"))
-#         log.debug(request.args.get("hub.verify_token"))
-#         return "traca_verifiy_token", 200
+        data_manager = DatastoreManager()
+        return data_manager.update_comment(comment_data).get_result_json()
 
 
 @app.route('/callback/<input>')
